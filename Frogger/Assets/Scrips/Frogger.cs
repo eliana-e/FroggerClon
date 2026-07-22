711472100e9d8c37ed8777646ef0ffa8df572faa
eliana-e <eeeliana.004@gmail.com>
2026-07-22 00:13:54 -0300

    basic game logic
    basic game logic and UI added. basic game done

--- Frogger/Assets/Scrips/Frogger.cs
+++ Frogger/Assets/Scrips/Frogger.cs
@@ -6,10 +6,15 @@ public class Frogger : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public Sprite idleSprite;
     public Sprite leapSprite;
+    public Sprite deadSprite;
 
+    private Vector3 spawnPosition;
+
+    private float farthestRow;
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spawnPosition = transform.position;
     }
     private void Update()
     {
@@ -34,11 +39,39 @@ public class Frogger : MonoBehaviour
         }
     }
 
-    private void Move (Vector3 direcction)
+    private void Move (Vector3 direction)
     {
-        Vector3 destination = transform.position + direcction;
+        Vector3 destination = transform.position + direction;
+        Collider2D barrier = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
+        Collider2D platform = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+        Collider2D obstacle = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Obstacle"));
 
-        StartCoroutine(Leap(destination));
+        if ( barrier!= null)
+        {
+            return;
+        }
+
+        if (platform != null)
+        {
+            transform.SetParent(platform.transform);
+        } else {
+            transform.SetParent(null);
+        }
+
+        if (obstacle != null && platform == null)
+        {
+            transform.position= destination;
+            Death();
+        }
+        else
+        {
+            if(destination.y > farthestRow)
+            {
+                farthestRow = destination.y;    
+                FindObjectOfType<GameManager>().AdvancedRow();
+            }
+            StartCoroutine(Leap(destination));
+        }
     }
 
     private IEnumerator Leap(Vector3 destination)
@@ -59,5 +92,36 @@ public class Frogger : MonoBehaviour
 
         transform.position = destination;
         spriteRenderer.sprite= idleSprite;
+    }
+
+    public void Death()
+    {
+        StopAllCoroutines();
+
+        transform.rotation= Quaternion.identity;
+        spriteRenderer.sprite = deadSprite;
+        enabled= false;
+
+        FindObjectOfType<GameManager>().Died();
+    }
+
+    public void Respawn()
+    {
+        StopAllCoroutines();
+
+        transform.rotation = Quaternion.identity;
+        transform.position = spawnPosition;
+        farthestRow= spawnPosition.y;
+        spriteRenderer.sprite = idleSprite;
+        gameObject.SetActive(true);
+        enabled= true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (enabled && other.gameObject.layer== LayerMask.NameToLayer("Obstacle") && transform.parent == null)
+        {
+            Death();
+        }
     }
 }
